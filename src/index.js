7fb42672edc6f3cb548a866e4fe127a91f67c9ef
IANULA CONDREA <andreww.steel69@gmail.com>
2026-01-22 15:50:12 +0200

    link all modules in index and add listener for contact

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import "./style.css";
-import "./home.js";
-import "./menu.js";
-import "./contact.js";
 
 import loadHome from "./home.js";
 import loadMenu from "./menu.js";
+import loadContact from "./contact.js";
 
 loadHome();
 
-const homeBtn = document.getElementById("home-btn");
-const menuBtn = document.getElementById("menu-btn");
+document.getElementById("home-btn").classList.add("active");
 
-homeBtn.addEventListener("click", () => loadHome());
-menuBtn.addEventListener("click", () => loadMenu());
+const navButtons = document.querySelectorAll("nav button");
+
+navButtons.forEach(button => {
+
+    button.addEventListener("click", e => {
+        
+        navButtons.forEach(btn => btn.classList.remove("active"));
+
+        e.target.classList.add("active");
+
+        const id = e.target.id;
+
+        if (id === "home-btn") loadHome();
+        if (id === "menu-btn") loadMenu();
+        if (id === "contact-btn") loadContact();
+    });
+});
